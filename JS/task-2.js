2f55ee4da50d138ed9050384c2931d26998b4848
Roman <vrbox@i.ua>
2024-06-05 17:09:17 +0300

    Complete

---
+++ JS/task-2.js
@@ -0,0 +1,10 @@
+
+function getShippingMessage(country, price, deliveryFee) {
+    const mess = `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+    return mess;
+}
+
+console.log("\n********** Task-2 ************");
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20)); 
---
+++ JS/task-3.js
@@ -0,0 +1,12 @@
+
+function getElementWidth(content, padding, border) {
+    const widthContent = Number.parseFloat(content);
+    const widthPadding = Number.parseFloat(padding);
+    const widthBorder = Number.parseFloat(border);
+    return widthContent + 2 * (widthPadding + widthBorder);
+}
+
+console.log("\n********** Task-3 ************");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
